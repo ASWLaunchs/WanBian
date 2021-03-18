4f5770bb76f65194dbb0182f951dbd2d9dd45c48
ASWLaunchs <1732747279@qq.com>
2021-03-18 16:19:08 +0800

    updated __init__.py file

--- __init__.py
+++ __init__.py
@@ -1,4 +1,16 @@
 import pandas as pd
-from io import StringIO
-data = "col1,col2,col3\na,b,1\na,2\nc,d,3"
-print(pd.read_csv(StringIO(data)))
+import numpy as np
+
+
+def ReadData():
+    # data be used to storage QUestion col data which in csv.
+    data = pd.read_csv("data/testing.csv", usecols=[0, 1], converters={
+        "Question": str, "Answer": str}, encoding='GB18030')
+    dataQuestion = np.array(data['Question'].values.tolist())
+    dataAnswer = np.array(data['Answer'].values.tolist())
+
+def main():
+    ReadData()
+
+if __name__ == "__main__":
+    main()
