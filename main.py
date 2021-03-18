b23ac23d65dd9751b1f71e2cc6df54998b7d50dc
ASWLaunchs <1732747279@qq.com>
2021-03-18 22:08:25 +0800

    I can't sure my file structure is correct , just design it like this

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import pandas as pd
 import numpy as np
+import wanbian_html.wanbian_html as wh
+import wanbian_word.wanbian_word as ww
 
 
-def ReadData():
+def ReadCsv():
     # data be used to storage QUestion col data which in csv.
-    data = pd.read_csv("data/testing.csv", usecols=[0, 1], converters={
+    data = pd.read_csv("resource/demo/demo.csv", usecols=[0, 1], converters={
         "Question": str, "Answer": str}, encoding='GB18030')
     dataQuestion = np.array(data['Question'].values.tolist())
     dataAnswer = np.array(data['Answer'].values.tolist())
 
+
 def main():
-    ReadData()
+    ReadCsv()
+    wh.CreateHTML()
+    ww.Output()
+
 
 if __name__ == "__main__":
     main()
