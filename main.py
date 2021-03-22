6f2abd33625c739d48f446c33d4157a0d05e7b5d
ASWLaunchs <1732747279@qq.com>
2021-03-22 22:54:57 +0800

    overhauled codes , optimize the file path setting part

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import wanbian_html.html_testing as wHtml
 import wanbian_word.word_testing as wWord
 
 
-# Init() be used in initialize related parameters.
-def Init(ProjectName):
+# CreateFile() be used in initialize related parameters about file.
+def CreateFile(ProjectName):
     # step 1: create a file path which be used storage csv file according to the project name.
     # step 2: create a csv file according to the project name.
     # step 3: process all initialization variables.
@@ -25,7 +25,7 @@ def ReadCsv(filePath):
 
 
 def main():
-    filePath = input("please input your project name")
+    filePath = input("please input your project name:")
     dataQuestion, dataAnswer = ReadCsv(filePath)
     wHtml.CreateHTML(dataQuestion, dataAnswer)
     wWord.CreateWord(dataQuestion, dataAnswer)
