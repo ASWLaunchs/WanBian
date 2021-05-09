85d46d75bef45130ac92ac43ed843b875e989f01
ASWLaunchs <1732747279@qq.com>
2021-05-09 23:55:14 +0800

    added serial array number.

--- packages/wanbian_word/word_testing.py
+++ packages/wanbian_word/word_testing.py
@@ -9,6 +9,7 @@ class wordTesting:
         # initialize extra class.
         self.WHash = wHash.Hash()
 
+    # CreateHashArr() array the file name according to time hash value.
     def CreateHashArr(self, projectName, projectTitle, dataQuestion, dataAnswer, count):
         while count > 0:
             count -= 1
@@ -21,5 +22,15 @@ class wordTesting:
             document.add_page_break()
             document.save("data/"+projectName+"/"+self.WHash.hash()+".docx")
 
-    def CreateSerialArr(self, projectName, dataQuestion, dataAnswer, count):
-        pass
+    # CreateSerialArr() array the file name according to serial number value.
+    def CreateSerialArr(self, projectName, projectTitle, dataQuestion, dataAnswer, count):
+        while count > 0:
+            count -= 1
+            document = Document()
+            document.add_heading(projectTitle, 0)
+            for i in range(0, len(dataQuestion)):
+                document.add_paragraph(
+                    dataQuestion[i]+"\n"+dataAnswer[i] + "\n", style='List Number'
+                )
+            document.add_page_break()
+            document.save("data/"+projectName+"/"+projectTitle+count+".docx")
