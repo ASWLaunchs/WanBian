6f9dbf7e2c798a0632233352233b2a12392bf332
ASWLaunchs <47150186+ASWLaunchs@users.noreply.github.com>
2021-05-20 15:07:45 +0800

    Update wanbian_markdown.py

--- packages/wanbian_markdown/wanbian_markdown.py
+++ packages/wanbian_markdown/wanbian_markdown.py
@@ -1,1 +1,10 @@
 import markdown
+
+class Markdown:
+	def __init__(self):
+		pass
+	
+	#Create() is used to create a html by modularize 
+	def Create(self,markdownContent):
+		
+		html = markdown.markdown(markdownContent)
